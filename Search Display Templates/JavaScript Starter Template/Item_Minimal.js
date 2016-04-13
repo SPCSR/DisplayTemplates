b60b65de2a6677da06fd9e9754bb0f4256fb9c1d
Elio Struyf <info@estruyf.be>
2016-04-13 10:21:12 +0200

    Updated the starter item template
    This template requires less configuration for the template file
    registration.

--- Search Display Templates/JavaScript Starter Template/Item_Minimal.js
+++ Search Display Templates/JavaScript Starter Template/Item_Minimal.js
@@ -1,69 +1,61 @@
-/**
- * Javascript Starter Template - a minimal Display Template in JavaScript
- * @author Anatoly Mironov @mirontoli
- * blog post: https://chuvash.eu/2016/04/13/minimal-display-template/
- * @date 2016-04-13
- */
-
-
-(function() {
-  //config contains variables that are defined in one place
+(function () {
+  // Config contains variables that are defined in one place
   var config = {
-    templateName: "Item_Minimal",
-    customFolder: "Contoso",
-    fileName: 'Item_Minmal.js'
-  }
-  //url to your display template
-  var templateUrl = ['~sitecollection', '_catalogs', 'masterpage', 
-    'Display Templates', 'Search', config.customFolder, config.fileName].join('\u002f');
-
-  //this is the function you want to care about. Here is the markup for every Result Item defined
-  function getHtml(plainItem) {
-    return ['<div>', title, '</div>'].join('');
-  }
-  //this is the built-in function, "_Minimal" is my change. You can keep it.
-  //perhaps it is possible to refactor this function
-  function DisplayTemplate_Minimal(ctx) {
-    var cachePreviousTemplateData = ctx.DisplayTemplateData;
-    ctx.DisplayTemplateData = {
-      'TemplateUrl': templateUrl,
-      'TemplateType': 'Item',
-      'TargetControlType': 'SearchResults',
-      'ManagedPropertyMapping': {
-        'Title':['Title']
-      }
-    };
-    var cachePreviousItemValuesFunction = ctx.ItemValues;
-    ctx.ItemValues = function(slotOrPropName) {
-      return Srch.ValueInfo.getCachedCtxItemValue(ctx, slotOrPropName)
-    };
-    ctx.ItemValues = cachePreviousItemValuesFunction;
-    ctx.DisplayTemplateData = cachePreviousTemplateData;
-    //here you map Search result to you plain object
-    var plainItem = { 
-      title: $getItemValue(ctx, "Title")
-    }
-    //here you get the html markup for your plain object
-    return getHtml(plainItem);
-  }
-
-  //the remainder is just the boilerplate for registering your display template, normal and for MDS
-  window.RegisterTemplate_Minimal = function() {
-    var srchLoaded = Srch && Srch.U && Srch.registerRenderTemplateByName;
-    if (srchLoaded) {
-      Srch.U.registerRenderTemplateByName(config.templateName, DisplayTemplate_Minimal);
-    }
-
-    if ("undefined" != typeof (Srch) &&
-      "undefined" != typeof (Srch.U) &&
-      typeof(Srch.U.registerRenderTemplateByName) == "function") {
-      Srch.U.registerRenderTemplateByName(templateUrl, DisplayTemplate_Minimal);
-    }
+    propertyMappings: { 'Path':null, 'Title':['Title'] }
   };
+  var templateUrl;
 
-  window.RegisterTemplate_Minimal();
+  var register = function () {
+      if ("undefined" !== typeof (Srch) && "undefined" !== typeof (Srch.U) && typeof (Srch.U.registerRenderTemplateByName) === "function") {
+              Srch.U.registerRenderTemplateByName(templateUrl, render);
+          }
+    };
+    render = function (ctx) {
+      // Display template data
+      var cachePreviousTemplateData = ctx.DisplayTemplateData;
+        ctx.DisplayTemplateData = {
+        'TemplateUrl': templateUrl,
+        'TemplateType': 'Item',
+        'TargetControlType': ['SearchResults', 'Content Web Parts'],
+        'ManagedPropertyMapping': config.propertyMappings
+        };
+        var cachePreviousItemValuesFunction = ctx.ItemValues;
+        ctx.ItemValues = function(slotOrPropName) {
+                return Srch.ValueInfo.getCachedCtxItemValue(ctx, slotOrPropName);
+        };
 
-  if (typeof(RegisterModuleInit) == "function" && typeof(Srch.U.replaceUrlTokens) == "function") {
-    RegisterModuleInit(Srch.U.replaceUrlTokens(templateUrl), window.RegisterTemplate_Minimal);
+      // Retrieve managed property data
+      var path = $getItemValue(ctx, 'Path');
+      var title = $getItemValue(ctx, 'Title');
+
+      // HTML markup for an item
+      var htmlMarkup = String.format( '<div>' +
+                        '<a href="{0}" title="{1}">{1}</a>' +
+                                        '</div>', path, title);
+
+        // Caching
+        ctx.ItemValues = cachePreviousItemValuesFunction;
+        ctx.DisplayTemplateData = cachePreviousTemplateData;
+
+        // Return the HTML markup
+        return htmlMarkup;
+    };
+
+  // Retrieve all the loaded scripts
+  var allScripts = document.getElementsByTagName("script");
+  // Get the last script file (this is the current DT file)
+    var scriptUrl = allScripts[allScripts.length - 1].src;
+    if (scriptUrl.indexOf('/_catalogs/') > 0) {
+      // Remove the query string 
+      if (scriptUrl.indexOf('?') > 0) {
+        scriptUrl = scriptUrl.split("?")[0];
+      }
+      // Insert the site collection token
+      templateUrl = '~sitecollection' + scriptUrl.substr(scriptUrl.indexOf('/_catalogs/'))
+    // Register the template to load
+    register();
+      if (typeof (RegisterModuleInit) === "function" && typeof(Srch.U.replaceUrlTokens) === "function") {
+          RegisterModuleInit(Srch.U.replaceUrlTokens(templateUrl), register);
+      }
   }
 })();
