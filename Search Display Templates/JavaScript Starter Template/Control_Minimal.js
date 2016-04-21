6bdd4451b00daaf6bc1d21751757f4c2f75c9637
Elio Struyf <info@estruyf.be>
2016-04-21 14:10:07 +0200

    Bug fix for the starter control template

--- Search Display Templates/JavaScript Starter Template/Control_Minimal.js
+++ Search Display Templates/JavaScript Starter Template/Control_Minimal.js
@@ -1,6 +1,11 @@
 (function () {
     'use strict';
     
+    // Config contains variables that are defined in one place
+    var config = {
+        /* IMPORTANT: update these settings before uploading the file to the master page gallery */
+        template: 'control_minimal.js'
+    };
 	var templateUrl;
 
 	var register = function () {
@@ -44,23 +49,34 @@
 		    return iStr.join('');
 		};
 	
-	// MDS needs to start on the head
-	// Retrieve all the loaded scripts
-	var allScripts = document.head.getElementsByTagName("script");
-	// Get the last script file (this is the current DT file)
-    var scriptUrl = allScripts[allScripts.length - 1].src;
-    if (scriptUrl.indexOf('/_catalogs/') > 0) {
+	/* DO NOT REMOVE THE FOLLOWING LINES OF CODE */
+    // MDS needs to start on the head
+    // Retrieve all the loaded scripts
+    var allScripts = document.head.getElementsByTagName("script");
+    var scriptUrl = null;
+    var scriptNr = allScripts.length;
+    while(scriptNr--) {
+        var crntScript = allScripts[scriptNr];
+        if (crntScript.src !== null) {
+            // Check if the right script is retrieved based on the filename of the template
+            if (crntScript.src.indexOf('/_catalogs/') > 0 && crntScript.src.toLowerCase().indexOf(config.template.toLowerCase()) > 0) {
+                scriptUrl = crntScript.src;
+                break;
+            }
+        }
+    }    
+    if (scriptUrl !== null) {
 	    // Remove the query string 
 	    if (scriptUrl.indexOf('?') > 0) {
-	    	scriptUrl = scriptUrl.split("?")[0];
+	        scriptUrl = scriptUrl.split("?")[0];
 	    }
 	    // Insert the site collection token
 	    templateUrl = '~sitecollection' + scriptUrl.substr(scriptUrl.indexOf('/_catalogs/'));
 	    templateUrl = decodeURI(templateUrl);
-		// Register the template to load
-		register();
+	    // Register the template to load
+	    register();
 	    if (typeof (RegisterModuleInit) === "function" && typeof(Srch.U.replaceUrlTokens) === "function") {
 	        RegisterModuleInit(Srch.U.replaceUrlTokens(templateUrl), register);
 	    }
-	}
+    }
 })();
